3a4da0d3ee4a0612b8eeb159ad6691caebe47985
1101099728@qq.com <142xyh753869>
2020-09-23 18:26:52 +0800

    添加美女图片

--- script/other/beauty.js
+++ script/other/beauty.js
@@ -2,6 +2,8 @@ const electron = require('electron');
 const { BrowserWindow, ipcMain } = require('electron')
 const path = require('path');
 const fetch = require('../../common/fetch')
+const config = require('../../common/config')
+const {makeDir} =require('../../common/util')
 let mainWindow;
 
 async function createWindow() {
@@ -15,13 +17,11 @@ async function createWindow() {
             devTools: true, //  Boolean (可选) - 是否开启 DevTools. 如果设置为 false, 则无法使用
             // Boolean (可选) - 是否集成Node，默认为false
             nodeIntegration: true,
-            // preload: path.join(__dirname, 'zhihu-img-preload.js'),
             webviewTag: true,
         }
     })
-
+    mainWindow.webContents.openDevTools()
     await mainWindow.loadFile(path.join(__dirname,'/index.html'))
-
     // Emitted when the window is closed.
     mainWindow.on('closed', function () {
         // Dereference the window object, usually you would store windows
@@ -30,10 +30,26 @@ async function createWindow() {
         mainWindow = null
     })
 }
+function saveImgBase64Data (base_64_url) {
+    var fs = require("fs");  // 引入fs模块
+    makeDir(path.join(config.outputDir, `美女`))
+    var distPath = path.join(config.outputDir, `美女/${Date.now()}.png`)
+    var base64 = base_64_url.replace(/^data:image\/\w+;base64,/, ""); //去掉图片base64码前面部分data:image/png;base64
+    var dataBuffer = Buffer.alloc(base64, 'base64'); //把base64码转成buffer对象，
+    console.log(distPath)
+    fs.writeFile(distPath,dataBuffer,function(err){//用fs写入文件
+        if(err){
+            console.log('写入失敗', err);
+        }else{
+            console.log('写入成功！');
+        }
+    });
+
+}
 
 ipcMain.on('callFunction', function (event, data) {
-    if(data.functionName === 'toggleDevTools') {
-        mainWindow.webContents.openDevTools()
+    if (data.name === 'base64') {
+       saveImgBase64Data(data.data)
     }
 })
 
